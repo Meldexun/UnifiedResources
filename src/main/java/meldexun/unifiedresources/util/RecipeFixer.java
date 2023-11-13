57da21471d1f9c86be976efad50ef61ea57431d3
Meldexun <27779321+Meldexun@users.noreply.github.com>
2023-11-13 23:00:41 +0100

    Remove unnecessary special treatment of vanilla recipes

--- src/main/java/meldexun/unifiedresources/util/RecipeFixer.java
+++ src/main/java/meldexun/unifiedresources/util/RecipeFixer.java
@@ -6,26 +6,14 @@ import org.apache.logging.log4j.Logger;
 import meldexun.unifiedresources.UnifiedResources;
 import meldexun.unifiedresources.api.IRecipeMutableResult;
 import meldexun.unifiedresources.config.UnifiedResourcesConfig;
-import meldexun.unifiedresources.reflection.ReflectionField;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.AbstractCookingRecipe;
 import net.minecraft.item.crafting.IRecipe;
-import net.minecraft.item.crafting.ShapedRecipe;
-import net.minecraft.item.crafting.ShapelessRecipe;
-import net.minecraft.item.crafting.SingleItemRecipe;
-import net.minecraft.item.crafting.SmithingRecipe;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
 public class RecipeFixer {
 
 	private static final Logger LOGGER = LogManager.getLogger();
-
-	private static final ReflectionField<ItemStack> COOKING_RECIPE__RESULT = new ReflectionField<>(AbstractCookingRecipe.class, "field_222143_e", "result");
-	private static final ReflectionField<ItemStack> SINGLE_ITEM_RECIPE__RESULT = new ReflectionField<>(SingleItemRecipe.class, "field_222132_b", "result");
-	private static final ReflectionField<ItemStack> SMITHING_RECIPE__RESULT = new ReflectionField<>(SmithingRecipe.class, "field_234839_c_", "result");
-	private static final ReflectionField<ItemStack> SHAPED_RECIPE__RESULT = new ReflectionField<>(ShapedRecipe.class, "field_77575_e", "result");
-	private static final ReflectionField<ItemStack> SHAPELESS_RECIPE__RESULT = new ReflectionField<>(ShapelessRecipe.class, "field_77580_a", "result");
 
 	public static int recipesChecked = 0;
 	public static int outputsUpdated = 0;
@@ -61,32 +49,9 @@ public class RecipeFixer {
 					}
 				}
 			}
-		} else if (recipe instanceof AbstractCookingRecipe) {
-			fixVanillaRecipe(recipe, COOKING_RECIPE__RESULT);
-		} else if (recipe instanceof SingleItemRecipe) {
-			fixVanillaRecipe(recipe, SINGLE_ITEM_RECIPE__RESULT);
-		} else if (recipe instanceof SmithingRecipe) {
-			fixVanillaRecipe(recipe, SMITHING_RECIPE__RESULT);
-		} else if (recipe instanceof ShapedRecipe) {
-			fixVanillaRecipe(recipe, SHAPED_RECIPE__RESULT);
-		} else if (recipe instanceof ShapelessRecipe) {
-			fixVanillaRecipe(recipe, SHAPELESS_RECIPE__RESULT);
 		} else {
 			for (RecipeOutputFixer recipeOutputFixer : RecipeOutputFixer.getRecipeOutputFixers(recipe.getClass())) {
 				recipeOutputFixer.fixRecipeOutput(recipe, recipe);
-			}
-		}
-	}
-
-	private static void fixVanillaRecipe(IRecipe<?> recipe, ReflectionField<ItemStack> resultAccessor) {
-		ItemStack stack = resultAccessor.get(recipe);
-		ItemStack newStack = UnifiedResources.getReplacement(stack);
-
-		if (newStack != null) {
-			resultAccessor.set(recipe, newStack);
-			outputsUpdated++;
-			if (UnifiedResourcesConfig.SERVER_CONFIG.debug.get()) {
-				LOGGER.info("Recipe: Replaced {} with {} in {}", stack.getItem().getRegistryName(), newStack.getItem().getRegistryName(), recipe.getId());
 			}
 		}
 	}
