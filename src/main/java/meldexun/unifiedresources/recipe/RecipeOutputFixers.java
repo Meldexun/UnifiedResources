87231c3326a8d68f85a45c3e2649926d00a494e9
Meldexun <27779321+Meldexun@users.noreply.github.com>
2024-10-15 12:45:51 +0200

    Fix vanilla recipes not being unified

--- src/main/java/meldexun/unifiedresources/recipe/RecipeOutputFixers.java
+++ src/main/java/meldexun/unifiedresources/recipe/RecipeOutputFixers.java
@@ -3,8 +3,10 @@ package meldexun.unifiedresources.recipe;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.Nullable;
@@ -16,11 +18,19 @@ import org.apache.logging.log4j.Logger;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.Util;
 
 public class RecipeOutputFixers {
 
 	private static final Logger LOGGER = LogManager.getLogger();
 	private static final Map<Class<?>, List<RecipeOutputFixer>> RECIPE_OUTPUT_FIXERS = new ConcurrentHashMap<>();
+	private static final Set<String> VANILLA_RECIPE_RESULT_NAMES = Util.make(new HashSet<>(), set -> {
+		set.add("field_222143_e"); // AbstractCookingRecipe.result
+		set.add("field_222132_b"); // SingleItemRecipe.result
+		set.add("field_234839_c_"); // SmithingRecipe.result
+		set.add("field_77575_e"); // ShapedRecipe.result
+		set.add("field_77580_a"); // ShapelessRecipe.result
+	});
 
 	public static List<RecipeOutputFixer> getRecipeOutputFixers(Class<?> classToCheck) {
 		List<RecipeOutputFixer> cachedRecipeOutputFixers = RECIPE_OUTPUT_FIXERS.get(classToCheck);
@@ -60,7 +70,9 @@ public class RecipeOutputFixers {
 			return null;
 		}
 
-		if (!StringUtils.containsIgnoreCase(field.getName(), "result") && !StringUtils.containsIgnoreCase(field.getName(), "output")) {
+		if (!VANILLA_RECIPE_RESULT_NAMES.contains(field.getName())
+				&& !StringUtils.containsIgnoreCase(field.getName(), "result")
+				&& !StringUtils.containsIgnoreCase(field.getName(), "output")) {
 			return null;
 		}
 
